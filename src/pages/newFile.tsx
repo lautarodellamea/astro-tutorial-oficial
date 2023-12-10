5c93e0550d8e15a0642faf5d91c894785a43e727
Lautaro Della Mea <lautarodm98@gmail.com>
2023-12-09 22:11:05 -0300

    sección 4

---
+++ src/pages/newFile.tsx
@@ -0,0 +1,8 @@
+import BaseLayout from '../layouts/BaseLayout.astro';
+import { pageTitle } from './index.astro';
+
+<Fragment>
+<BaseLayout pageTitle={pageTitle}>
+<h2>Mi impresionante subtítulo del blog</h2>
+</BaseLayout>
+</Fragment>;
